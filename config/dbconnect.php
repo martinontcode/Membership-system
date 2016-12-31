da4829a2954a3397f5874062b5f38d4ec47c0466
Martin <martin.onton@gmail.com>
2016-12-31 16:39:31 +0200

    Uploaded config.php

--- config/dbconnect.php
+++ config/dbconnect.php
@@ -1,8 +1,17 @@
-<?php
+<?php 
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/* Credentials */
+$servername = "Insert your servername";
+$username = "Insert your database user";
+$password = "Insert your database password";
+$database = "login";
 
+/* Connection */
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+/* If connection fails for some reason */
+if (!$conn) {
+	die("Database connection failed: ".mysqli_connect_error());
+}
+
+?>
