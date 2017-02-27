d0332853fbee43d7e6b93d95db20ac7271a0eb24
Martin <martin.onton@gmail.com>
2017-02-27 14:05:27 +0200

    Form for password reset page.

---
+++ forgot.php
@@ -0,0 +1,32 @@
+<?php include_once 'header.php'; ?>
+
+<div class="container">
+
+    <!-- Login form -->
+    <div class="loginForm">
+        <form  action="forgot.php" name="forgotpsw" class="form-forgot" method="post">
+            <h3 class="cnt">Forgot your password?</h3>
+            <hr class="colorgraph">
+            
+            <p class="">Enter your email. We'll email instructions on how to reset your password.</p>
+            
+            <label for="email">E-mail<span class="red">*</span>:</label>
+            <input type="email" name="email" id="email" placeholder="E-mail" class="input form-control" autocomplete="off" required autofocus><br>
+            
+            <!-- If there is an error it will be shown. --> 
+            <?php if(!empty($_SESSION['message'])): ?>
+                <div class="alert alert-danger alert-container" id="alert">
+                    <strong><center><?php echo htmlentities($_SESSION['message']) ?></center></strong>
+                    <?php unset($_SESSION['message']); ?>
+                </div>
+            <?php endif; ?>
+            
+            <input type="submit"  name="forgot" value="Send e-mail" class="btn btn-lg btn-block submit" /> 
+            
+        </form>
+
+
+
+    </div>  <!-- End loginForm-->
+
+</div>  
