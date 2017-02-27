e8ccd60ec7bce42fdb23cc7d4020c341bb2a6cf0
Martin <martin.onton@gmail.com>
2017-02-27 11:32:59 +0200

    New error and success message system.

--- views/registrationForm.php
+++ views/registrationForm.php
@@ -15,9 +15,20 @@ include_once 'header.php';
             <input type="password" name="password" id="password" placeholder="Password" class="input form-control" autocomplete="off" required>
             <input type="password" name="password2" id="password2" placeholder="Re-enter password" class="input form-control" autocomplete="off" required><br>
             
-            <!-- Show messages -->
-            <?php if(!empty(@$_SESSION['errorMessage'])){
-            $registration->messages(); }?><br>
+            <!-- If there is an error it will be shown. --> 
+            <?php if(!empty($_SESSION['message'])): ?>
+                <div class="alert alert-danger alert-container" id="alert">
+                    <strong><center><?php echo htmlentities($_SESSION['message']) ?></center></strong>
+                    <?php unset($_SESSION['message']); ?>
+                </div>
+            <?php endif; ?>
+            <!-- If user account is created. -->
+            <?php if(!empty($_SESSION['SuccessMessage'])): ?>
+                <div class="alert alert-success alert-container" id="alert">
+                    <strong><center><?php echo htmlentities($_SESSION['SuccessMessage']) ?></center></strong>
+                    <?php unset($_SESSION['SuccessMessage']); ?>
+                </div>
+            <?php endif; ?>
                         
             <input type="submit"  name="registration" value="Sign Up" class="btn btn-lg btn-block submit" />  
         
