68ed081ca32215ebab755ccceabf0d9b92dce777
Martin <martin.onton@gmail.com>
2017-02-28 10:41:07 +0200

    Created verify.php
    If verify.php is opened from e-mail it will GET id and token key from
    URL, cross-reference this to database and if both are correct user is
    activated(Also in Database). Otherwise error alert is shown.

---
+++ verify.php
@@ -0,0 +1,21 @@
+<?php require_once 'header.php'; ?>
+
+<?php 
+    /* Require login.php to call login function */
+    require("classes/ClassRegistration.php");
+
+    /* Call for login function */
+    $verify = new Registration();
+    
+    $verify->Verify();
+?> 
+<div class="container container-content">
+    <?php if($verify->Verify() == TRUE): ?>
+    <h3 class="text-center">Account has been activated.</h3>
+    <p class="text-center">To login click on <a href="login.php" class="btn btn-primary btn-sm">Login</a></p>
+    <?php elseif($verify->Verify() == FALSE): ?>
+    <h3 class="text-center">There has been an error while activating your account.</h3>
+    <p class="text-center">Please contact support at <a href="mailto:support@membership.com?Subject=Konto%aktiveerimise%viga" class="link-blue">support@membership.com</a>.</p>
+    
+    <?php endif; ?>  
+</div>
